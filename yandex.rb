42afc940aff3eb1456c55010563dd67b83a61917
alex@laptop <karasev.alexey@laptop>
2015-03-22 11:26:48 +0300

    finished

--- yandex.rb
+++ yandex.rb
@@ -3,6 +3,9 @@ require 'nokogiri'
 class Entry
   attr_accessor :title, :url, :desc
   def to_csv
+    @url||=''
+    @title||=''
+    @desc||=''
     @url+';'+@title+';'+@desc+"\n"
   end
 end
@@ -16,27 +19,42 @@ def get_files
   files
 end
 
-path = '/media/storage/Dropbox/Temp/QYa/Гостевой пост идеи бизнеса — Яндекс  нашёлся 1 млн ответов.html'
-file = File.open(path, 'r')
-doc = Nokogiri::HTML(file)
-entries = []
-doc.css('div.serp-item__wrap').each do |item|
-  title_item = item.css('a.serp-item__title-link').first
-  if title_item
-    entry=Entry.new
-    entry.title=title_item.text
-    entry.url=title_item.attr 'href'
-    break if entry.url=~/yabs\.yandex\.ru/i #yandex ads
-    desc_item = item.css('.serp-item__text').first
-    desc = desc_item && desc_item.text
-    if desc
-      desc.gsub!('<b>','')
-      desc.gsub!('</b>','')
-    end
-    entry.desc=desc
-    entries << entry
+def export(entries)
+  f=File.open('output.csv', 'w')
+  entries.each do |e|
+    f.write e.to_csv
   end
+  f.close
 end
-file.close
-entries.each {|e| puts e.to_csv}
-# puts get_files
+
+def parse_file(path)
+  file = File.open(path, 'r')
+  doc = Nokogiri::HTML(file)
+  entries = []
+  doc.css('div.serp-item__wrap').each do |item|
+    title_item = item.css('a.serp-item__title-link').first
+    if title_item
+      entry=Entry.new
+      entry.title=title_item.text
+      entry.url=title_item.attr 'href'
+      break if entry.url=~/yabs\.yandex\.ru/i #yandex ads
+      desc_item = item.css('.serp-item__text').first
+      desc = desc_item && desc_item.text
+      if desc
+        desc.gsub!('<b>','')
+        desc.gsub!('</b>','')
+      end
+      entry.desc=desc
+      entries << entry
+    end
+  end
+  file.close
+  entries
+end
+
+entries = []
+files = get_files
+files.each do |f|
+  entries+=parse_file f
+end
+export entries
